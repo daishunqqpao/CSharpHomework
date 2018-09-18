abf4b9054720bc2ab876cb64a57750ae6d223ff7
舜 代 <qwerdaishun@qq.com>
2018-09-18 22:05:14 +0800

    第二次作业

--- CSharp-第二次作业/CSharp-第二次作业/Program.cs
+++ CSharp-第二次作业/CSharp-第二次作业/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharp_第二次作业
+namespace 第6题
 {
     class Program
     {
--- CSharp-第二次作业/第8题/Program.cs
+++ CSharp-第二次作业/第8题/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace 第8题
+namespace 第9题
 {
     class Program
     {

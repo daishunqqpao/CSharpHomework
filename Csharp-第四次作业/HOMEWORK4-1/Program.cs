d1d7f867e55374e5603ee8796b76dd55e463d6bf
舜 代 <qwerdaishun@qq.com>
2018-10-08 11:48:12 +0800

    第四次作业 难度较高

---
+++ Csharp-第四次作业/HOMEWORK4-1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_自定义闹钟
+{
+    
+
+   
+        public delegate void InformHandle(object sender);
+        public class CurrentTimeSetandJudge
+        {
+            public event InformHandle Timenow;
+
+            public void Time(int hour,int minute)
+            {
+                int wait = hour * 3600 + minute * 60;
+                int systemswait = wait * 1000;
+                int m = DateTime.Now.Hour;
+                int n = DateTime.Now.Minute;
+                 Console.WriteLine("约定时间还没有到，安心休息吧~");
+            while (hour != m)
+            {
+                int a = DateTime.Now.Hour;
+                m = a;
+            }
+            while(minute!=n)
+            {
+                int b = DateTime.Now.Minute;
+                n = b;
+
+            }
+                Ontime();//这个相当于是一个信号，当运行这个函数的时候会发出一个信号。
+            }
+            public virtual void Ontime()
+            {
+                if (Timenow != null)
+                {
+                    Timenow(this);
+                }
+            }
+        }
+        public class AlarmLauch
+        {
+            public  void WakeYouUp(object sender)
+            {
+                Console.WriteLine("起来啦！！！！！！！！！！！！！！！！！！！！！！");
+            }
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+        {
+            string hour = "";
+            string minute = "";
+            CurrentTimeSetandJudge a = new CurrentTimeSetandJudge();
+            AlarmLauch b = new AlarmLauch();
+            a.Timenow += new InformHandle(b.WakeYouUp);
+            Console.WriteLine("请先输入你想几时起来：(输完后得到提示之后再输入分钟,24小时制）");   
+            hour= Console.ReadLine();
+            int n = int.Parse(hour);
+            Console.WriteLine("知道啦！请再输入你几分钟起来：");
+            minute = Console.ReadLine();
+            int m = int.Parse(minute);
+            a.Time(n, m);
+            Console.ReadKey();
+
+        }
+
+         }
+}
---
+++ Csharp-第四次作业/HOMEWORK4-2/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOMEWORK4_2
+{
+    class Order
+    {
+
+    }
+    class OrderDetails : OrderService
+    {
+        public string ordernum
+        {
+            get { return ordernum; }
+            set { ordernum = value; }
+        }
+        public string goodname
+        {
+            get { return goodname; }
+            set { goodname = value; }
+        }
+        public string customername
+        {
+            get { return customername; }
+            set { customername = value; }
+        }
+        public OrderDetails(string ordernum,string goodname,string customername)
+            {
+            this.ordernum = ordernum;
+            this.goodname = goodname;
+            this.customername = customername;
+
+             }
+
+    }
+    class OrderService
+    {
+
+
+  
+
+
+        //将orderdetails对象放入该类集合
+
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "", b = "", c = "",f="";
+            OrderDetails p1 = new OrderDetails("1", "饺子", "张三");
+            OrderDetails p2 = new OrderDetails("2", "包子", "李四");
+            OrderDetails p3 = new OrderDetails("3", "馒头", "王五");
+            List<OrderService> order = new List<OrderService>();
+            order.Add(p1);
+            order.Add(p2);
+            order.Add(p3);
+            //添加订单(如果不添加此订单则会在删除时产生异常）
+            Console.WriteLine("请输入订单号：");
+            a = Console.ReadLine();
+            Console.WriteLine("请输入货物名称：");
+            b = Console.ReadLine();
+            Console.WriteLine("请输入客人姓名：");
+            c = Console.ReadLine();
+            OrderDetails pnew = new OrderDetails(a, b, c);
+            order.Add(pnew);
+            //删除订单
+            try
+            {
+
+                order.Remove(p1);
+                order.Remove(pnew);
+            }
+            catch
+            {
+                Console.WriteLine("删除订单出错啦！不存在新订单");
+            }
+
+            //查询订单
+            Console.WriteLine("请输入你要查询的订单号:");
+            f = Console.ReadLine();
+            if (p1.ordernum == f || p2.ordernum == f || p3.ordernum == f || pnew.ordernum == f) 
+            {
+                Console.WriteLine("存在此订单");
+            }
+            else
+            {
+                Console.WriteLine("不存在此订单");
+            }
+            //修改订单
+            try
+            {
+                Console.WriteLine("请输入新的订单号:");
+                p2.ordernum = Console.ReadLine();
+                Console.WriteLine("请输入新的货物名称：");
+                p2.goodname = Console.ReadLine();
+                Console.WriteLine("请输入新的客人名称：");
+                p2.customername = Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("修改订单失败");
+            }
+
+            
+
+
+
+
+
+
+
+        }
+    }
+}

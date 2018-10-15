289a8f0ab3aa000d85388cec389414839638eb62
舜 代 <qwerdaishun@qq.com>
2018-10-15 21:13:46 +0800

    第五次作业（ 看主函数 ）

---
+++ Csharp-第五次作业/Csharp-第五次作业/Program1/Program1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (graphics == null) graphics = this.CreateGraphics();
+                // string s = textBox1.Text;
+                // double d = double.Parse(s);
+                //double per2 = d;一直出错。。。无法强制转换获取文本值
+                drawCayleyTree1(10, 200, 310, 100, -Math.PI / 2);
+            }
+            catch {  }
+        }
+
+
+        private Graphics graphics;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+        void drawCayleyTree1(int n, double x0, double y0, double leng, double th)
+        {
+            if (n == 0) return;
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            drawLine1(x0, y0, x1, y1);
+            drawCayleyTree1(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree1(n - 1, x1, y1, per2 * leng, th - th2);
+        }
+        void drawLine1(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(
+                Pens.Blue,
+                (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (graphics == null) graphics = this.CreateGraphics();
+            drawCayleyTree2(10, 200, 310,100, -Math.PI / 2);
+
+        }
+
+        void drawCayleyTree2(int n, double x0, double y0,double leng, double th)
+        {
+            if (n == 0) return;
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            double x2 = x0 + leng * 2 * Math.Cos(th);
+            double y2 = y0 + leng * 2 * Math.Sin(th);
+            drawLine2_1(x0, y0, x1, y1);
+            drawCayleyTree2(n - 1, x1, y1, per1 * leng, th + th1);
+           drawLine2_2(x0+10, y0+20, x2, y2);
+            drawCayleyTree2(n - 1, x2, y2, per2 * leng, th-th2);
+            
+        }
+    
+        void drawLine2_1(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(
+                Pens.Red,
+                (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+        void drawLine2_2(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(
+                Pens.Green,
+                (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+    }
+}
---
+++ Csharp-第五次作业/Csharp-第五次作业/Program1/Program2(order renew)/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2_order_renew_
+{
+    class Order
+    {
+
+    }
+    class OrderDetails : OrderService
+    {
+        public string ordernum
+        {
+            get;
+            set;
+        }
+        public string goodname
+        {
+            get;
+            set;
+        }
+        public string customername
+        {
+            get;
+            set;
+        }
+        public double ordervalue
+        {
+            get;
+            set;
+        }
+        public OrderDetails(string ordernum, string goodname, string customername, double ordervalue)
+        {
+            this.ordernum = ordernum;
+            this.goodname = goodname;
+            this.customername = customername;
+            this.ordervalue = ordervalue;
+
+        }
+
+    }
+    class OrderService
+    {
+
+
+
+
+
+        //将orderdetails对象放入该类集合
+
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "", b = "", c = "", e = "", f = "";
+            OrderDetails p1 = new OrderDetails("1", "饺子", "张三", 10000);
+            OrderDetails p2 = new OrderDetails("2", "包子", "李四", 20000);
+            OrderDetails p3 = new OrderDetails("3", "馒头", "王五", 1000);
+            List<OrderService> order = new List<OrderService>();
+            order.Add(p1);
+            order.Add(p2);
+            order.Add(p3);
+            //添加订单(如果不添加此订单则会在删除时产生异常）
+            Console.WriteLine("请输入订单号：");
+            a = Console.ReadLine();
+            Console.WriteLine("请输入货物名称：");
+            b = Console.ReadLine();
+            Console.WriteLine("请输入客人姓名：");
+            c = Console.ReadLine();
+            Console.WriteLine("请输入订单价格：");
+            e = Console.ReadLine();
+            double d = double.Parse(e);
+            OrderDetails pnew = new OrderDetails(a, b, c, d);
+            order.Add(pnew);
+            //删除订单
+            try
+            {
+
+                order.Remove(p1);
+                order.Remove(pnew);
+            }
+            catch
+            {
+                Console.WriteLine("删除订单出错啦！不存在新订单");
+            }
+
+            //查询订单
+            Console.WriteLine("请输入你要查询的订单号:");
+            f = Console.ReadLine();
+            if (p1.ordernum == f || p2.ordernum == f || p3.ordernum == f || pnew.ordernum == f)
+            {
+                Console.WriteLine("存在此订单");
+            }
+            else
+            {
+                Console.WriteLine("不存在此订单");
+            }
+            //修改订单
+            try
+            {
+                Console.WriteLine("请输入新的订单号:");
+                p2.ordernum = Console.ReadLine();
+                Console.WriteLine("请输入新的货物名称：");
+                p2.goodname = Console.ReadLine();
+                Console.WriteLine("请输入新的客人名称：");
+                p2.customername = Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("修改订单失败");
+            }
+            try
+            {
+                //按照商品名称查询(linq)
+                Console.WriteLine("请输入查询的商品名称:");
+                string mm = Console.ReadLine();
+                var n = from OrderDetails god in order
+                        where god.goodname == mm
+                        select god;
+
+                foreach (var m in n)
+                {
+                    Console.WriteLine(m.ordernum + m.goodname + m.customername + m.ordervalue);
+
+
+                }
+                //按照客户名字查询（linq）
+                Console.WriteLine("请输入查询的客户姓名:");
+                string nn = Console.ReadLine();
+                var sb = from OrderDetails god in order
+                         where god.goodname == nn
+                         select god;
+
+                foreach (var m in sb)
+                {
+                    Console.WriteLine(m.ordernum + m.goodname + m.customername + m.ordervalue);
+                }
+                //按照订单号查询
+                Console.WriteLine("请输入查询的订单号:");
+                string pp = Console.ReadLine();                
+                var bs = from OrderDetails god in order
+                         where god.goodname == nn
+                         select god;
+
+                foreach (var m in bs)
+                {
+                    Console.WriteLine(m.ordernum + m.goodname + m.customername + m.ordervalue);
+                }
+                //查询订单金额大于1万元的订单
+                var money = from OrderDetails god in order
+                            where god.ordervalue >= 10000
+                            select god;
+                foreach(var m in money)
+                {
+                    Console.WriteLine(m.ordernum + m.goodname + m.customername + m.ordervalue);
+                }
+            }
+            catch { }
+        }
+    }
+}
